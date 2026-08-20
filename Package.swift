28cc7a3d5b3233e417162688c522b8a421d81c5c
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 10:44:12 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -31,11 +31,11 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-css",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(name: .css, targets: [.css]),
@@ -47,8 +47,14 @@ let package = Package(
         .library(name: "CSS Test Support", targets: ["CSS Test Support"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-foundations/swift-css-html-render.git", branch: "main"),
-        .package(url: "https://github.com/swift-foundations/swift-css-html-layout-render.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-foundations/swift-css-html-render.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-foundations/swift-css-html-layout-render.git",
+            branch: "main"
+        ),
         .package(url: "https://github.com/swift-foundations/swift-html-render.git", branch: "main"),
         .package(url: "https://github.com/swift-standards/swift-css-standard.git", branch: "main"),
     ],
